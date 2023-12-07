5ab953d91011423f437df7d8c8f5906c9add123d
norviah <21983700+Norviah@users.noreply.github.com>
2023-12-06 19:47:14 -0800

    feat: render the theme selector within the application's top-right corner

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,9 +1,15 @@
+import { ThemeSelector } from '@/components/ThemeSelector';
 import type { LayoutProps } from '@/types/components/LayoutProps';
 
 export default function DashboardLayout(props: LayoutProps): JSX.Element {
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
-      <main className="container">{props.children}</main>
-    </div>
+    <>
+      <div className="fixed right-0 top-0 p-4">
+        <ThemeSelector />
+      </div>
+      <div className="flex min-h-screen flex-col space-y-6">
+        <main className="container">{props.children}</main>
+      </div>
+    </>
   );
 }
