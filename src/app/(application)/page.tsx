6726f32fc9f19eccc710fd4ad2ea5b46eb09c326
norviah <21983700+Norviah@users.noreply.github.com>
2023-12-06 20:27:00 -0800

    feat(components): add a component to render links from the config file

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,3 +1,11 @@
-export default function HomePage(): JSX.Element {
-  return <span>hello world</span>;
+import { Menu } from '@/components/Menu';
+
+export default function Home(): JSX.Element {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <Menu />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,29 @@
+import { config } from '@/config';
+import type { Command } from '@/types/Config';
+
+export function Menu(): JSX.Element {
+  const entries: [string, Command[]][] = Object.entries(config.links);
+
+  return (
+    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+      {entries.map(([category, commands]) => {
+        return (
+          <div key={category}>
+            <div className="flex flex-col gap-7">
+              {commands.map((command: Command, index: number) => {
+                return (
+                  <a key={`${category}-${command.name}-${index}`} href={command.url}>
+                    <div className="flex gap-4 text-sm">
+                      <span className="text-foreground">{command.key}</span>
+                      <span className="text-muted-foreground transition-colors hover:text-foreground">{command.name}</span>
+                    </div>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
