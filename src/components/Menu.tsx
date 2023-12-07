52aba92825b3a50fcc7c447ebfc2b3a39beb8b88
norviah <21983700+Norviah@users.noreply.github.com>
2023-12-06 20:57:30 -0800

    refactor(components/Menu): refactor grid layout to wrap each category in a single div

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,22 +5,20 @@ export function Menu(): JSX.Element {
   const entries: [string, Command[]][] = Object.entries(config.links);
 
   return (
-    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
       {entries.map(([category, commands]) => {
         return (
-          <div key={category}>
-            <div className="flex flex-col gap-7">
-              {commands.map((command: Command, index: number) => {
-                return (
-                  <a key={`${category}-${command.name}-${index}`} href={command.url}>
-                    <div className="flex gap-4 text-sm">
-                      <span className="text-foreground">{command.key}</span>
-                      <span className="text-muted-foreground transition-colors hover:text-foreground">{command.name}</span>
-                    </div>
-                  </a>
-                );
-              })}
-            </div>
+          <div key={category} className="flex flex-col gap-7">
+            {commands.map((command: Command, index: number) => {
+              return (
+                <a key={`${category}-${command.name}-${index}`} href={command.url}>
+                  <div className="flex gap-4 text-sm">
+                    <span className="text-foreground">{command.key}</span>
+                    <span className="text-muted-foreground transition-colors hover:text-foreground">{command.name}</span>
+                  </div>
+                </a>
+              );
+            })}
           </div>
         );
       })}
