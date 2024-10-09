10b85a16aaedea9114b15db353f702e4cc7b5c02
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-08 21:47:58 -0700

    style(components): ensure `LinkIcon` renders above all elements

--- src/components/LinkIcon.tsx
+++ src/components/LinkIcon.tsx
@@ -12,7 +12,7 @@ export type LinkIcon = {
 
 export function LinkIcon({ icon: Icon, route }: LinkIcon): JSX.Element {
   return (
-    <Link href={route} className='fixed top-0 right-0 p-5'>
+    <Link href={route} className='absolute top-0 right-0 z-[999] p-5'>
       <Button variant='ghost' className='p-2 hover:bg-transparent'>
         <Icon className='size-5 lg:size-6 xl:size-7' />
       </Button>
